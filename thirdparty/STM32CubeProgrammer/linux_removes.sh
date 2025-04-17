cceb9b9c60c95a76ee4bb638e4142f2618b6fc42
brad112358 <brad112358@gmail.com>
2025-04-17 01:17:12 -0500

    update STM32CubeProgrammer to 2.0.19 (Linux)

---
+++ thirdparty/STM32CubeProgrammer/linux_removes.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# This script shows what was removed from a minimal Linux install
+# (with STM32Trusted PackageCreator unchecked) to generate the linux folder.
+
+rm -rf linux/IzPackLocaleEnabledXdgDesktopIconScript.sh
+rm -rf linux/.installationinformation
+rm -rf linux/api
+rm -rf linux/bin/DA_Default_Config
+rm -rf linux/bin/ExternalLoader
+rm -rf linux/bin/FastROM_Data_Base
+rm -rf linux/bin/HSM
+rm -rf linux/bin/jre
+rm -rf linux/bin/libcrypto.so
+rm -rf linux/bin/libssl.so
+rm -rf linux/bin/libstp11_SAM.so
+rm -rf linux/bin/libstp11_SAM.so.conf
+rm -rf linux/bin/OBL
+rm -rf linux/bin/PMIC_Data_Base
+rm -rf linux/bin/STM32CubeProgrammer
+rm -rf linux/bin/STM32CubeProgrammer.l4j.ini
+rm -rf linux/bin/STM32CubeProgrammerLauncher
+rm -rf linux/bin/STM32_KeyGen_CLI
+rm -rf linux/bin/STM32_Programmer.sh
+rm -rf linux/bin/STM32_SigningTool_CLI
+rm -rf linux/bin/STM32WLScripts
+rm -rf linux/bin/version
+rm -rf linux/doc
+rm -rf linux/Drivers
+rm -rf linux/lib/libCubeProgrammer_API.so
+rm -rf linux/lib/libCubeProgrammer_API.so.1
+rm -rf linux/lib/libFileManager.so.1
+rm -rf linux/lib/libicudata.so.52
+rm -rf linux/lib/libicuuc.so.52
+rm -rf linux/lib/libpsa_sdm.so
+rm -rf linux/lib/libssl.so.1.1
+rm -rf linux/lib/libstp11_SAM.so
+rm -rf linux/lib/libstp11_SAM.so.conf
+rm -rf linux/lib/libxerces-c-3.2.so
+rm -rf linux/SVD
+rm -rf linux/uninstaller
+rm -rf linux/util
