0ad8d55d5ebd4cba66d8dadb59240de1e5c19455
jlpoltrack <41841496+jlpoltrack@users.noreply.github.com>
2025-11-24 02:34:53 -0500

    metadata, adjust xr receiver multiband

--- assets/mLRS_metadata.py
+++ assets/mLRS_metadata.py
@@ -265,10 +265,10 @@ g_targetDict = {
         'rx-radiomaster-rp4td-2400' : {
             'chipset' : 'esp32',
         },
-        'rx-radiomaster-xr1-900' : {
+        'rx-radiomaster-xr1' : {
             'chipset' : 'esp32c3',
         },
-        'rx-radiomaster-xr4-900' : {
+        'rx-radiomaster-xr4' : {
             'chipset' : 'esp32',
         },
     },
