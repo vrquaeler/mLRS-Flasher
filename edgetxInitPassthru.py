9e75e233d3c171c80e8e03c8cb6406a9139422e2
jlpoltrack <41841496+jlpoltrack@users.noreply.github.com>
2025-11-29 16:49:38 -0500

    edgetxInitPassthru.py, find serial, remove edgetx name check on posix

--- edgetxInitPassthru.py
+++ edgetxInitPassthru.py
@@ -31,11 +31,7 @@ def find_radio_serial_ports():
     radioportList = []
     for port in portList:
         if port.vid == 0x0483 and port.pid == 0x5740:
-            if os.name == 'posix': # we do have more info on this os
-                if port.manufacturer == 'EdgeTX':
-                    radioportList.append(port.device)
-            else:
-                radioportList.append(port.device)
+            radioportList.append(port.device)
     return radioportList
 
 
